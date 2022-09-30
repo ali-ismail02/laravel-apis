62d901c58d9ca2def5758ad9a11765a4c8daa24f
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-30 15:29:33 +0300

    Added number splitting api

--- app/Http/Controllers/apis_controller.php
+++ app/Http/Controllers/apis_controller.php
@@ -52,4 +52,29 @@ class apis_controller extends Controller
             "message" => $str
         ]);
     }
+    function splitValues(){
+        $nb = 100;
+        $flag = 0;
+        if($nb < 0){
+            $nb = -$nb;
+            $flag=1;
+        }
+        $array = [];
+        while($nb >= 1){
+            $value = $nb % 10;
+            $nb = $nb/10;
+            $array[] = $value;
+        }
+        $array = array_reverse($array);
+        for($i = count($array)-1,$j=0; $i>=0;$i--,$j++){
+            if($flag){
+                $array[$j] = -($array[$j] * pow(10,$i));
+            }else $array[$j] = $array[$j] * pow(10,$i);
+        }
+
+        return response()->json([
+            "status" => 1,
+            "message" => $array
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\apis_controller;
 
 Route::get("/sort",[apis_controller::class,"sortString"]);
+Route::get("/split",[apis_controller::class,"splitValues"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
